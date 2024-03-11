5f671c67b70c58ede0aa04a23029ddabaf5afb53
agnel0108 <55734523+agnel0108@users.noreply.github.com>
2024-03-11 15:48:43 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -2,9 +2,9 @@
 
 int main() {
     for (int i = 1; i <= 5; ++i) {
-        std::cout << i << std::endl
+        std::cout << i << std::endl;
     }
-    std::cout << "hello" << std::endl
+    std::cout << "hello" << std::endl;
 
     return 0;
 }
